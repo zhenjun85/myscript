6b88714d7f8cdb8defd514a98b7cb069b7c204ad
zhenjun.zhang <zhenjun.zhang@spreadtrum.com>
2016-10-02 08:43:10 +0800

    upload the new change

---
+++ patches/power/ShutdownAnimation.java
@@ -0,0 +1,36 @@
+package com.android.server.power;
+
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.view.IWindowManager;
+import android.util.Slog;
+import java.io.File;
+
+public class ShutdownAnimation {
+    private static final String TAG = "ShutdownAnimationUtil";
+    boolean playShutdownAnimation() {
+        File fileDefault = new File("/system/media/shutdownanimation.zip"); 
+        File file = new File("/data/theme/overlay/shutdownanimation.zip");
+        boolean hasShutdownAnimation = file.exists() || fileDefault.exists();
+        if (!hasShutdownAnimation) {
+            return false;
+        }
+        IWindowManager wm = IWindowManager.Stub.asInterface(ServiceManager.getService("window"));
+        try {
+            wm.setForceOrientation(0);
+        } catch (RemoteException e) {
+            Slog.e(TAG, "stop orientation failed!", e);
+        }
+
+        String[] bootcmd = {"bootanimation", "shutdown"} ;
+        try {
+            Slog.i(TAG, "exec the bootanimation ");
+            SystemProperties.set("service.bootanim.exit", "0");
+            Runtime.getRuntime().exec(bootcmd);
+        } catch (Exception e) {
+            Slog.e(TAG,"bootanimation command exe err!");
+        }
+        return true;
+    }
+}
---
+++ test/a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -n 1
+echo -n 3
+sleep 1
+echo -ne "\b"
+echo -n 2
